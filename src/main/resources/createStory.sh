60b243c8756f85d68e9776f8536509c2d5288988
sheldon <sheldon@ece.ubc.ca>
2013-02-26 15:00:47 -0800

    Created a script for outputting a complete story as a graph (embedded with sequence diagrams).

---
+++ src/main/resources/createStory.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd ../../../metis-output/ftrace/sequence_diagrams/
+cp ../../../src/main/resources/sequence.pic .
+
+ls *.pic > list
+sed 's/\.pic//' < list > list2
+sed 's/sequence//' < list2 > list 
+for i in `cat list`; do pic2plot -Tps "$i".pic > "$i".ps ; done
+rm list2
+rm list
+
+ls *.ps > list
+sed 's/\.ps//' < list > list2 
+for i in `cat list2`; do convert "$i".ps "$i".png ; done
+rm list
+rm list2
+
+ls story_* > list
+for i in `cat list`
+do
+echo $i
+
+sed 's/digraph G {/digraph G {\
+\
+  compound=true;\
+  rankdir="LR";\
+  ranksep=1\.25;\
+  label="METIS EPISODE RELATIONS";\
+\
+  node \[shape=plaintext\, fontsize=36\];\
+\
+  bgcolor=white;\
+  edge \[arrowsize=1, color=black\];\
+  /g' <$i >image_graph.dot
+
+done
+rm list
+
+sed "s/E\([0-9]*\) \[ label=\"E\([0-9]*\)\" \];/subgraph cluster_E\1 \{label=\"E\1\"; labelloc=\"b\"; style=invis; E\1\_icon\};NEWLINEE\1\_icon \[label=\"\"\, shape=box\, shapefile=\"\1\.png\"\];NEWLINE/g" image_graph.dot >flip.dot 
+
+sed 's/NEWLINE/\
+  /g' flip.dot >image_graph.dot
+
+sed "s/E\([0-9]*\) ->/E\1\_icon ->/g" image_graph.dot > flip.dot
+sed "s/-> E\([0-9]*\)/-> E\1\_icon/g" flip.dot >image_graph.dot
+
+rm flip.dot
+rm *.ps
+rm sequence.pic
+
+dot -Tpng -oimage_graph.png image_graph.dot
+cd -
