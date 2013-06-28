c6249fc8715560f5900634dd3a68f0b42ef6990b
mohamed ali <mohammedali.saisdubai@gmail.com>
2013-06-28 15:01:30 -0700

    fixed problems with api.

--- src/main/java/com/clematis/core/episode/Episode.java
+++ src/main/java/com/clematis/core/episode/Episode.java
@@ -11,6 +11,10 @@ public class Episode {
 	private EpisodeTrace trace;
 	private String dom;
 
+	public Episode() {
+
+	}
+
 	/*
 	 * public Episode(EpisodeSource source) { trace = new EpisodeTrace(); }
 	 */
