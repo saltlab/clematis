779d21f324d9987263c020aa4c81c3631c3ab7a4
sheldon <sheldon@ece.ubc.ca>
2014-02-13 16:14:07 -0800

    Removed unused files.

--- src/main/resources/trial.js
+++
@@ -1,9 +0,0 @@
-
-		
-document.write('<script type="text/javascript" src="jquery-1.9.1.js"></script>');
-document.write('<script type="text/javascript" src="jquery-ui-1.10.2.custom.js"></script>');
-document.write('<script type="text/javascript" src="jquery.tipsy.js"></script>');
-document.write('<link rel="stylesheet" type="text/css" href="css/jquery-ui-1.10.2.custom.css">');
-document.write('<link rel="stylesheet" type="text/css" href="css/tipsy.css">');
-//document.write('<script type="text/javascript" src="toolbar.js"></script>');
-		
